bd3e80c886f8739daf64ed7d367facbf36c4e6ad
soul1se <kuboebredis@yandex.ru>
2026-05-08 02:24:49 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const DONATE_CONFIG = {
+  wallet: "4100119528033348",
+  amount: 250,
+  title: "Поддержать проект",
+  description: "Спасибо за поддержку! Нажми кнопку ниже, чтобы отправить донат.",
+  paymentTarget: "Донат проекту",
+  successUrl: "https://github.com/soul1se/donate"
+};
