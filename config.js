3ff5aa17491a3bc58e216869aebcfc219c85ba8e
soul1se <kuboebredis@yandex.ru>
2026-05-08 02:36:18 +0300

    Update config.js
    update 1.0

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const DONATE_CONFIG = {
   wallet: "4100119528033348",
   amount: 350,
+
   title: "Поддержать проект",
   description: "Спасибо за поддержку! Нажми кнопку ниже, чтобы отправить донат.",
+
   paymentTarget: "Донат проекту",
-  successUrl: "https://github.com/soul1se/donate"
+
+  successUrl: "https://soul1se.github.io/donate/"
 };
